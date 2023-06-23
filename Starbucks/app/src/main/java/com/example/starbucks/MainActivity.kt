a0955255aaa387cda2e8231b39b03a628cd36f8a
unique0902 <rla0591@gmail.com>
2023-06-23 13:01:12 +0900

    Feat: add bottom navigation

--- Starbucks/app/src/main/java/com/example/starbucks/MainActivity.kt
+++ Starbucks/app/src/main/java/com/example/starbucks/MainActivity.kt
@@ -2,10 +2,62 @@ package com.example.starbucks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.starbucks.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initBottomNavigation()
+    }
+
+    private fun initBottomNavigation() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main_frm, WebtoonFragment())
+            .commitAllowingStateLoss()
+
+
+        binding.mainBnv.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.webtoonFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, WebtoonFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+
+                R.id.recommendFinishFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, RecommendFinishFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.bestChallengeFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, BestChallengeFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.myFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, MyFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.moreFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, MoreFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+            }
+            false
+        }
+
+        binding.mainBnv.itemIconTintList = null
     }
 }
